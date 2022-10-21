e6d467468d1a79bdc13ef9fe5172576fc1e90afa
eric60305 <33591012+eric60305@users.noreply.github.com>
2022-10-21 15:27:15 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,17 @@
-rm -rf alist* data/ #Uncomment this line to update
-if [ ! -f "alist" ];then
-curl -L https://github.com/alist-org/alist/releases/download/v3.0.1/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
-tar -zxvf alist.tar.gz
-rm -f alist.tar.gz
-mv alist-linux-musl-amd64 alist
-fi
-./alist server
-
+# rm -rf alist* data/ #Uncomment this line to update
 # if [ ! -f "alist" ];then
 #   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+# #   curl -L https://github.com/alist-org/alist/releases/download/v3..1/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
 #   tar -zxvf alist.tar.gz
 #   rm -f alist.tar.gz
-# fi
+#   mv alist-linux-musl-amd64 alist
+#  fi
 # ./alist server --no-prefix
+
+rm -rf alist* data/ #Uncomment this line to update
+if [ ! -f "alist" ];then
+  curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+  tar -zxvf alist.tar.gz
+  rm -f alist.tar.gz
+fi
+./alist server --no-prefix
